518b2e2ad36dd9727161899532fa315aeacee3fc
MUBASHRA IRSHAD <184015269+mubashra21c@users.noreply.github.com>
2025-01-28 14:50:10 +0000

    INITIAL COMMIT

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        playfair:"'Playfair', serif",
+        lato:"'Lato', sans-serif"
+
+      }
+    },
+  },
+  plugins: [],
+}
+
